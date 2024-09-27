270a5016cf6be258271b7fde9761b4c803ce4711
Jimmy Van Sise <jimmyvansise@hotmail.com>
2024-09-27 10:35:46 -0700

    feat(players values): 1st batch of players, change PK to an autoincrement SERIAL

--- db/migration/V0_1__create_players_table.sql
+++ db/migration/V0_1__create_players_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE players (
-    player_id UUID PRIMARY KEY,
+    player_id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     nickname VARCHAR(50) NOT NULL,
---
+++ db/migration/V0_2__insert_players_values.sql
@@ -0,0 +1,12 @@
+INSERT INTO players (first_name, last_name, nickname, year_retired)
+VALUES ('Dak', 'Prescott', '', NULL),
+('Tom', 'Brady', '', 2023),
+('Joe', 'Montana', '', 1995),
+('Kordell', 'Stewart', 'Slash', 2007),
+('Troy', 'Aikman', '', 2000),
+('Emmitt', 'Smith', '', 2005),
+('Michael', 'Irvin', 'Playmaker', 2000),
+('Deion', 'Sanders', 'Primetime', 2005),
+('Larry', 'Allen', '', 2009),
+('Dan', 'Marino', '', 2000),
+('Rex', 'Grossman', '', 2012)
