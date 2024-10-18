647b3dc93a19f79eb4984d539cbae1fb7a587538
Jimmy Van Sise <jimmyvansise@hotmail.com>
2024-10-18 11:18:24 -0700

    chore(update cors)

--- src/rest-api/index.ts
+++ src/rest-api/index.ts
@@ -38,7 +38,7 @@ export const createServer = () => {
 
     const server = express();
     server.use(cors({
-        origin: 'http://127.0.0.1:3000'
+        origin: 'http://localhost:3000'
     }));
     /*
         app.use(cors({
